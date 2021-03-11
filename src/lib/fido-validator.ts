883f1247520e68f0afca93159b09cd5f359dc102
GabrielMeyer <meyergabriel@live.de>
2021-03-11 16:16:16 +0100

    Initial commit for FIDO2

---
+++ src/lib/fido-validator.ts
@@ -0,0 +1,30 @@
+import { WebAuthnType } from './definitions';
+import { FidoHelper } from './fido-helper';
+
+export namespace FidoValidator {
+  export function validate(clientData: any, expectations: any, type: WebAuthnType): void {
+    validateType(clientData, type);
+    validateChallenge(clientData, expectations);
+    validateOrigin(clientData, expectations);
+  }
+
+  function validateType(clientData: any, type: WebAuthnType): void {
+    if (clientData.type !== type) {
+      throw new Error(`Wrong type of webauthn! Expected 'webauthn.create' - Received ${clientData.type}`);
+    }
+  }
+
+  function validateChallenge(clientData: any, expectations: any): void {
+    if (clientData.challenge !== FidoHelper.trimBase64(expectations.challenge)) {
+      throw new Error(
+        `Challenges are different! Expected ${expectations.challenge} - Received ${clientData.challenge}`
+      );
+    }
+  }
+
+  function validateOrigin(clientData: any, expectations: any): void {
+    if (clientData.origin !== expectations.origin) {
+      throw new Error(`Origins are different! Expected ${expectations.origin} - Received ${clientData.origin}`);
+    }
+  }
+}
